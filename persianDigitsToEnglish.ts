d0c153e4b7aeecef05eb763269eefde0876d76e4
rubilk <milad.b.n.77@gmail.com>
2023-01-15 11:35:33 +0330

    first commit

---
+++ persianDigitsToEnglish.ts
@@ -0,0 +1,15 @@
+const persianDigitsToEnglish = (digit: string) => {
+  const persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"];
+  const arabicNumbers = ["١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩", "٠"];
+  const englishNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  return digit
+    .split("")
+    .map(
+      (c) =>
+        englishNumbers[persianNumbers.indexOf(c)] ||
+        englishNumbers[arabicNumbers.indexOf(c)] ||
+        c
+    )
+    .join("");
+};
